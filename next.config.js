83e96dc193a0b9f0d80c471d94c4d71447d3d09b
Luciano Marzoni <lucianomarzoni@gmail.com>
2025-08-20 12:09:32 -0300

    Initial project configuration - Rolled back nextra and netra-theme-docs dependencies in package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+//module.exports = withNextra()
+export default withNextra()
