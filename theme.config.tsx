13c9ed97b85f202e20ecc7ab7830079d9ee14613
Luciano Marzoni <lucianomarzoni@gmail.com>
2025-08-20 12:06:34 -0300

    Modified theme - changed project name and added logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+  <>
+    <img 
+      src="/osTicket_logo_400x400.png" 
+      alt="osTicket Logo" 
+      width={25} 
+      height={25} 
+      style={{ marginRight: "6px", verticalAlign: "middle"}}
+    />
+    <span>osTicket Documentation | Luciano Marzoni</span>
+  </>
+),
+//<img src="osTicket_logo_400x400.png" width="25" height="25"></img>
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
