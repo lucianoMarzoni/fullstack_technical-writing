c4a78085d99efd5d9bebedfaeb51b11eb30981c2
Luciano Marzoni <lucianomarzoni@gmail.com>
2025-08-21 23:01:51 -0300

    Added pages to be developed

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,16 +3,16 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
-  <>
-    <img 
-      src="/osTicket_logo_400x400.png" 
-      alt="osTicket Logo" 
-      width={25} 
-      height={25} 
-      style={{ marginRight: "6px", verticalAlign: "middle"}}
-    />
-    <span>osTicket Documentation | Luciano Marzoni</span>
-  </>
+    <>
+      <img 
+        src="/osTicket_logo_400x400.png" 
+        alt="osTicket Logo" 
+        width={25} 
+        height={25} 
+        style={{ marginRight: "6px", verticalAlign: "middle"}}
+      />
+      <span>osTicket Documentation | Luciano Marzoni</span>
+    </>
   ),
   project: {
     link: 'https://github.com/lucianoMarzoni/fullstack_technical-writing/',
